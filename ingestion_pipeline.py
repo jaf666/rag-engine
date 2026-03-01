7d64c9d59fc04627a9c87ef8be798d4858e3c182
Juanma <thejuanmit@gmail.com>
2026-03-01 11:28:58 +0100

    basic rag system completed succesfully

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -1,10 +1,13 @@
 import os
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
-from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
-from dotenv import load_dotenv
+from langchain_ollama import OllamaLLM, OllamaEmbeddings
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 
-# load_dotenv()
  
 def load_files(data_path="data"):
     if not os.path.exists(data_path):
@@ -31,7 +34,7 @@ def load_files(data_path="data"):
     return documents
 
 def chunk_files(documents, chunk_size=800, chunk_overlap=0):
-    text_splitter = CharacterTextSplitter(
+    text_splitter = RecursiveCharacterTextSplitter(
         chunk_size = chunk_size,
         chunk_overlap=chunk_overlap
     )
@@ -50,6 +53,37 @@ def chunk_files(documents, chunk_size=800, chunk_overlap=0):
     return chunks
 
 
+def create_vector_store(chunks):
+    # Switching to a dedicated embedding model to ensure semantic accuracy and lower latency.
+    # Generative models (like Gemma/Llama) are not optimized for vector transformations.
+    embeddings = OllamaEmbeddings(
+                model="nomic-embed-text",
+                )
+    vectorstore = InMemoryVectorStore.from_documents(
+        chunks,
+        embedding=embeddings,
+    )       
+
+    return vectorstore
+
+def create_rag_chain(vectorstore):
+    llm = OllamaLLM(model="gemma3")
+    template = """ 
+    Answer the question based only on the following context:
+    {context}
+
+    Question: {question}
+    """
+    prompt = ChatPromptTemplate.from_template(template)
+    
+    # Using LCEL to express query
+    chain = (
+        {"context": vectorstore.as_retriever(), "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    return chain
 
 def main():
     print("")
@@ -58,7 +92,16 @@ def main():
     
     # Chunking the files
     chunks = chunk_files(test_files)
+    
     # Embedding and Storing in Vector DB
+    vector_store = create_vector_store(chunks)
+
+    rag_chain = create_rag_chain(vector_store)
+
+    question = "What is the main topic about SpaceX?"
+    answer = rag_chain.invoke(question)
+
+    print(f"Final annswer: {answer}")
 
 
 if __name__ == "__main__":
